08ba8175fcaa8541f3e64e40c5104905baedf044
Elomidas <victor96@hotmail.fr>
2018-03-14 15:57:58 +0100

    Node constructor OK

--- src/Node.cpp
+++ src/Node.cpp
@@ -2,8 +2,8 @@
 // Created by elomidas on 14/03/18.
 //
 
-#include "include/Node.h"
+#include "../include/Node.h"
 
-public Node(const unsigned int ) {
-
+Node::Node(const unsigned int id, const unsigned int quantity) : m_id(id), m_quantity(quantity) {
+    //Nothing
 }
